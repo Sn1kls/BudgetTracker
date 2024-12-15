f487c247f233a298d9bd561ac6fc9f0fc92b839e
Sn1kls <macks.chigrin@gmail.com>
2024-12-15 15:03:28 +0200

    Initial Commit

---
+++ BudgetTracker/BudgetTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetTrackerApp.swift
+//  BudgetTracker
+//
+//  Created by macbook on 15.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct BudgetTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
